17480983305e00f19714abbbc2f0673aa15f60fd
nishant3197 <69510958+nishant3197@users.noreply.github.com>
2021-06-24 20:16:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+//for mouse click
+var all_buttons = document.querySelectorAll("button");
+var no_of_elements = all_buttons.length;
+
+for (var i=0; i<no_of_elements; ++i){
+
+    all_buttons[i].addEventListener("click", function(){
+        var buttonClicked = this.innerHTML;
+        createSoundOnChar(buttonClicked);
+    });
+}
+
+//for key pressing
+document.addEventListener("keydown",function (keyPressed){
+    createSoundOnChar(keyPressed.key);
+    
+});
+
+//create a sound
+function createSoundOnChar(character){
+    var elementSound;
+    switch(character){
+        case "w" : 
+                elementSound = "sounds/tom-1.mp3";
+                break;
+            
+        case "a": 
+            elementSound = "sounds/tom-2.mp3";
+            break;
+        
+        case "s": 
+            elementSound = "sounds/tom-3.mp3";
+            break;
+        
+        case "d": 
+            elementSound = "sounds/tom-4.mp3";
+            break;
+        
+        case "j": 
+            elementSound = "sounds/crash.mp3";
+            break;
+        
+        case "k": 
+            elementSound = "sounds/kick-bass.mp3";
+            break;
+        
+        case "l": 
+            elementSound = "sounds/snare.mp3";
+            break;
+        
+        default:
+            console.log(character);
+    }
+        var drumSound = new Audio(elementSound);
+        drumSound.play();
+}
